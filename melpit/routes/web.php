2c244c14712d57beaddd3da2507ed0dbe0f0934e
zkenkenz <kenji.dnt.11291225.kenjikenji@gmail.com>
2021-06-15 12:24:28 +0900

    マルチログイン

--- melpit/routes/web.php
+++ melpit/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->namespace('Admin')->name('admin.')->group(function(){
+    Auth::routes(['register' => false, 'confirm' => false, 'reset' => false, 'verify' => false]);
+    Route::get('/home', 'AdminHomeController@index')->name('admin_home');
+});
